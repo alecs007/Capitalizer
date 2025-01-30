231dfa0e77b4086458ffd4e17ab982f2c29220ac
Alex Rotar <96844939+alecs007@users.noreply.github.com>
2025-01-30 23:06:39 +0200

    Modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import "./App.css";
-import Header from "./components/Header/Header";
+import MainLayout from "./layout/MainLayout";
 
 function App() {
   return (
     <>
-      <Header />
+      <MainLayout />
     </>
   );
 }
---
+++ src/layout/MainLayout.jsx
@@ -0,0 +1,12 @@
+import "./MainLayout.css";
+import Header from "../components/Header/Header";
+
+const MainLayout = () => {
+  return (
+    <div className="main_layout">
+      <Header />
+    </div>
+  );
+};
+
+export default MainLayout;

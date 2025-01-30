cc56c5e0823f19802678b7e765b832b0bb66eb77
Alex Rotar <96844939+alecs007@users.noreply.github.com>
2025-01-30 22:00:56 +0200

    Modified

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,25 @@
 import "./Header.css";
+import github from "../../assets/github.png";
 
 const Header = () => {
   return (
     <div className="header">
-      <h1>Capitalizer</h1>
+      <div className="header_p">
+        <div className="header_p1">
+          <h1>Capitalizer</h1>
+        </div>
+        <div className="header_p2">
+          <div className="mode">All countries</div>
+          <div className="mode">Flags</div>
+          <div className="mode">1 of 4</div>
+          <div className="mode">Continents</div>
+        </div>
+      </div>
+      <div className="header_p3">
+        <a href="https://github.com/alecs007/Capitalizer" target="_blank">
+          <img src={github} alt="github" />
+        </a>
+      </div>
     </div>
   );
 };

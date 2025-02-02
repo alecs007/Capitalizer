89f2ca531a6214bd09c25e8a8025600ffea35c0b
Alex Rotar <96844939+alecs007@users.noreply.github.com>
2025-02-02 22:23:01 +0200

    Updated

--- src/pages/main_page/MainPage.jsx
+++ src/pages/main_page/MainPage.jsx
@@ -1,11 +1,23 @@
 import "./MainPage.css";
 import { useState, useEffect } from "react";
+import hint from "../../assets/hint.png";
+import trophy from "../../assets/trophy.png";
+import bronze from "../../assets/bronze_trophy.png";
+import silver from "../../assets/silver_trophy.png";
+import gold from "../../assets/gold_trophy.png";
+import diamond from "../../assets/diamond_trophy.png";
 
 const MainPage = () => {
   const [countries, setCountries] = useState({});
   const [randomCountry, setRandomCountry] = useState(null);
   const [excludedCountries, setExcludedCountries] = useState([]);
   const [finished, setFinished] = useState(false);
+  const [inputValue, setInputValue] = useState("");
+  const [isCorrect, setIsCorrect] = useState(false);
+  const [isIncorrect, setIsIncorrect] = useState(false);
+  const [disabled, setDisabled] = useState(false);
+  const [hintLenght, setHintLenght] = useState(1);
+  const [score, setScore] = useState(0);
 
   useEffect(() => {
     fetch("/data.json")
@@ -41,29 +53,103 @@ const MainPage = () => {
 
   console.log(excludedCountries);
 
+  const handleAnswer = () => {
+    if (inputValue.trim().length < 1) {
+      return;
+    }
+    setDisabled(true);
+    if (inputValue.toLowerCase() === randomCountry.capital.toLowerCase()) {
+      setIsCorrect(true);
+      setScore(score + 100);
+    } else {
+      setIsIncorrect(true);
+      if (score >= 50) {
+        setScore(score - 50);
+      }
+    }
+    setTimeout(() => {
+      getRandomCountry(countries);
+      setIsCorrect(false);
+      setIsIncorrect(false);
+      setInputValue("");
+      setDisabled(false);
+    }, 1000);
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter" && !disabled) {
+      handleAnswer();
+    }
+  };
+
+  const handleHint = () => {
+    if (hintLenght > randomCountry.capital.length || score < 100) return;
+    setHintLenght(hintLenght + 1);
+    setInputValue(randomCountry.capital.slice(0, hintLenght));
+    setScore(score - 100);
+  };
   return (
     <section className="main_page">
       {randomCountry && (
-        <div>
-          <h2>Random Country</h2>
-          <p>
-            <b>{randomCountry.name}</b> - {randomCountry.capital}{" "}
-            <img
-              src={randomCountry.flag}
-              alt={`Flag of ${randomCountry.name}`}
-              width="20"
-              height="15"
-            />
-          </p>
-          <button
-            onClick={() => getRandomCountry(countries)}
-            disabled={finished}
+        <div className="form">
+          <div
+            className={`country_name ${isCorrect ? "correct" : ""} ${
+              isIncorrect ? "incorrect" : ""
+            }`}
           >
-            Next
-          </button>
+            <h1>{randomCountry.name}</h1>
+          </div>
+          <div className="input_container">
+            <div className="input">
+              <input
+                type="text"
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
+                onKeyDown={(e) => handleKeyPress(e)}
+                placeholder="Your answer"
+                className="main_input"
+              />
+              <button
+                onClick={() => {
+                  handleAnswer();
+                  setHintLenght(1);
+                }}
+                disabled={disabled}
+                className="main_button"
+              >
+                Submit
+              </button>
+            </div>
+            <div className="status">
+              {finished && <h2>Finished</h2>}
+              {isIncorrect && (
+                <h2>
+                  Correct answer: <span>{randomCountry.capital}</span>{" "}
+                </h2>
+              )}
+            </div>
+          </div>
+          <div className="utilities">
+            <div className="hint" onClick={() => handleHint()}>
+              <img src={hint} alt="hint" />
+              <div className="hint_text">
+                <h2>HINT</h2>
+                <p>100pts</p>
+              </div>
+            </div>
+            <div className="score">
+              <div className="score_p1">{score} pts</div>
+              <div className="score_p2">
+                {score < 500 && <img src={trophy} />}
+                {score >= 500 && score < 1000 && <img src={bronze} />}
+                {score >= 1000 && score < 1500 && <img src={silver} />}
+                {score >= 1500 && score < 2000 && <img src={gold} />}
+                {score >= 2000 && <img src={diamond} />}
+              </div>
+            </div>
+          </div>
         </div>
       )}
-      {finished && <h2>Finished</h2>}
     </section>
   );
 };

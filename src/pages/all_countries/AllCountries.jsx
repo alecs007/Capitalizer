dbc6f8c864d3a1b0772ecdcee8592af6a25bcf62
Alex Rotar <96844939+alecs007@users.noreply.github.com>
2025-02-06 23:31:56 +0200

    Modified all countries mode

--- src/pages/all_countries/AllCountries.jsx
+++ src/pages/all_countries/AllCountries.jsx
@@ -1,9 +1,77 @@
 import "./AllCountries.css";
+import { useState, useEffect } from "react";
 
 const AllCountries = () => {
+  const [countries, setCountries] = useState({});
+  const [inputValues, setInputValues] = useState([]);
+  const [disabledInputs, setDisabledInputs] = useState([]);
+
+  useEffect(() => {
+    fetch("/data.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setCountries(data);
+
+        const allCountries = Object.values(data).flat();
+        setInputValues(Array(allCountries.length).fill(""));
+        setDisabledInputs(Array(allCountries.length).fill(false));
+      })
+      .catch((error) => console.error("Failed to fetch data", error));
+  }, []);
+
+  const handleInputChange = (index, value, capital) => {
+    const newInputValues = [...inputValues];
+    newInputValues[index] = value;
+    setInputValues(newInputValues);
+    if (value.toLowerCase() === capital.toLowerCase()) {
+      setInputValues((prevValues) => {
+        const newValues = [...prevValues];
+        newValues[index] = capital;
+        return newValues;
+      });
+      setDisabledInputs((prev) => {
+        const newDisabled = [...prev];
+        newDisabled[index] = true;
+        return newDisabled;
+      });
+    }
+  };
+
   return (
     <div className="all_countries">
-      <h1>all countries</h1>
+      {Object.keys(countries)
+        .sort((a, b) => countries[b].length - countries[a].length)
+        .map((continent) => (
+          <div key={continent}>
+            <div className="continent_name">{continent}</div>
+            <div className="continent_countries">
+              {countries[continent].map((country, countryIndex) => {
+                const globalIndex = Object.values(countries)
+                  .flat()
+                  .indexOf(country);
+                return (
+                  <div key={country.name} className="countries">
+                    <div className="countries_name">{country.name}</div>
+                    <input
+                      key={country.name}
+                      value={inputValues[globalIndex]}
+                      type="text"
+                      disabled={disabledInputs[globalIndex]}
+                      className="countries_input"
+                      onChange={(e) =>
+                        handleInputChange(
+                          globalIndex,
+                          e.target.value,
+                          country.capital
+                        )
+                      }
+                    />
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        ))}
     </div>
   );
 };
